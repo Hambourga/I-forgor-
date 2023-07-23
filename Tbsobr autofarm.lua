e90189bad8e51477446253e6b78fa0844c67cc99
hamburga <136784412+Hambourga@users.noreply.github.com>
2023-07-23 18:49:23 +0700

    Update Tbsobr autofarm.lua

--- Tbsobr autofarm.lua
+++ Tbsobr autofarm.lua
@@ -1,22 +1,29 @@
-while wait() do
+
+
+ game:GetService("RunService").RenderStepped:Connect(function()
      if _G.Autofarm then 
          spawn(function()
-             if game.Players.LocalPlayer.PlayerGui["Wave Counter"].WAVE.TextLabel.Text == "Wave: 0/0" then 
+             if game.Players.LocalPlayer.PlayerGui["Wave Counter"].WAVE.TextLabel.Text == "Wave: 0/0" or game.Players.LocalPlayer.PlayerGui["Wave Counter"].WAVE.TextLabel.Text == "Wave: 0/68408" then 
                  firetouchinterest(game.Workspace.Start.Part, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
                  firetouchinterest(game.Workspace.Start.Part, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
              elseif game.Players.LocalPlayer.PlayerGui["Wave Counter"].WAVE.TextLabel.Text == "Wave: 9/0" then 
                  fireclickdetector(game.Workspace.SoulKiller.Votes.ClickDetector)
             else
-                 setsimulationradius(100000, 100000)  
-                 for i, v in ipairs(game.Workspace:GetDescendants()) do
-                     if v:IsA("Humanoid") and not game.Players:GetPlayerFromCharacter(v.Parent) then
+                 sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", 69696969)
+                 sethiddenproperty(game.Players.LocalPlayer, "MaxSimulationRadius", math.huge)
+                 for i,v in ipairs(game.Workspace:GetDescendants()) do
+                     if v:IsA("Humanoid") and v.Parent:FindFirstChild("NPCTAG") ~= nil and v.Name == "Zombie" then
+                       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent:FindFirstChild("Torso").CFrame + Vector3.new(0,8,7)
+                       wait()
                        v.Parent:FindFirstChild("Head"):Destroy()
-                         v.Health = -math.huge
-                         v.MaxHealth = -math.huge
+                         v.Health = 0
+                         v.MaxHealth = 0
                      end
                  end
-                 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-116, 30, 518)
              end 
+             settings().Physics.AllowSleep = false
+              settings().Physics.ThrottleAdjustTime = math.huge-math.huge
          end)
      end 
-end
+ end)
+ 
