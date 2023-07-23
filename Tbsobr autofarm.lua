d20093922a7476192db95c39e72e3b9126b95db6
hamburga <136784412+Hambourga@users.noreply.github.com>
2023-07-23 15:31:10 +0700

    Update Tbsobr autofarm.lua

--- Tbsobr autofarm.lua
+++ Tbsobr autofarm.lua
@@ -1,4 +1,4 @@
-
+_G.Autofarm =
  
  while wait() do
      if _G.Autofarm then 
@@ -9,15 +9,14 @@
              elseif game.Players.LocalPlayer.PlayerGui["Wave Counter"].WAVE.TextLabel.Text == "Wave: 9/0" then 
                  fireclickdetector(game.Workspace.SoulKiller.Votes.Detector)
              else 
-                 wait(0.5)
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-127, 32, 332)
-                 setsimulationradius(100000, 100000)  
+                 setsimulationradius(1000000, 10000000)  
                  for i, v in ipairs(game.Workspace:GetDescendants()) do
                      if v:IsA("Humanoid") and not game.Players:GetPlayerFromCharacter(v.Parent) then
                          v.Health = 0
                          v.MaxHealth = 0
                      end
                  end
+                 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-116, 30, 518)
              end 
          end)
      end 
