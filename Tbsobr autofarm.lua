1826444635edfbd4355b5bb03aa3e37d59fedf78
hamburga <136784412+Hambourga@users.noreply.github.com>
2023-07-23 21:09:34 +0700

    Update Tbsobr autofarm.lua

--- Tbsobr autofarm.lua
+++ Tbsobr autofarm.lua
@@ -3,7 +3,7 @@
 
 local rensderpt
  rensderpt = game:GetService("RunService").RenderStepped:Connect(function()
-     if _G.Autofarm and _G.Slectbrlahlah == "Normal" then 
+     if _G.Autofarm  then 
          spawn(function()
              if game.Players.LocalPlayer.PlayerGui["Wave Counter"].WAVE.TextLabel.Text == "Wave: 0/0" or game.Players.LocalPlayer.PlayerGui["Wave Counter"].WAVE.TextLabel.Text == "Wave: 0/68408" then 
                  firetouchinterest(game.Workspace.Start.Part, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
